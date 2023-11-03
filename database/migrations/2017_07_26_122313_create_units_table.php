0183652f23b0adad9a5edfa6fea9207f63211bc5
Kawsar A <itskawsar@gmail.com>
2023-11-03 10:27:56 +0600

    fixed relationship fields for units migration

--- database/migrations/2017_07_26_122313_create_units_table.php
+++ database/migrations/2017_07_26_122313_create_units_table.php
@@ -15,13 +15,18 @@ return new class extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('business_id')->unsigned();
-            $table->foreign('business_id')->references('id')->on('business')->onDelete('cascade');
+            // $table->integer('business_id')->unsigned();
+            // $table->foreign('business_id')->references('id')->on('business')->onDelete('cascade');
+            $table->foreignId('business_id')->unsigned()->constrained('business')->onDelete('cascade');
+
             $table->string('actual_name');
             $table->string('short_name');
             $table->boolean('allow_decimal');
-            $table->integer('created_by')->unsigned();
-            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+
+            // $table->integer('created_by')->unsigned();
+            // $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('created_by')->unsigned()->constrained('users')->onDelete('cascade');
+
             $table->softDeletes();
             $table->timestamps();
         });
