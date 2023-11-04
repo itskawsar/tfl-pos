4da087bd68bb6898fb7470e01a1e9cfd38819196
Kawsar A <itskawsar@gmail.com>
2023-11-04 17:50:20 +0600

    disabled spatie roles and permission

--- database/migrations/2019_01_10_124645_add_account_permission.php
+++ database/migrations/2019_01_10_124645_add_account_permission.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Permission::create(['name' => 'account.access']);
+        // Permission::create(['name' => 'account.access']);
     }
 
     /**
