e9d2560a3efb2e11a7666e5104d5c473e3de0368
Kawsar A <itskawsar@gmail.com>
2023-11-04 17:52:22 +0600

    disabled activitylog table

--- database/migrations/2019_03_12_120336_create_activity_log_table.php
+++ database/migrations/2019_03_12_120336_create_activity_log_table.php
@@ -11,19 +11,19 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create(config('activitylog.table_name'), function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('log_name')->nullable();
-            $table->text('description');
-            $table->integer('subject_id')->nullable();
-            $table->string('subject_type')->nullable();
-            $table->integer('causer_id')->nullable();
-            $table->string('causer_type')->nullable();
-            $table->text('properties')->nullable();
-            $table->timestamps();
+        // Schema::create(config('activitylog.table_name'), function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('log_name')->nullable();
+        //     $table->text('description');
+        //     $table->integer('subject_id')->nullable();
+        //     $table->string('subject_type')->nullable();
+        //     $table->integer('causer_id')->nullable();
+        //     $table->string('causer_type')->nullable();
+        //     $table->text('properties')->nullable();
+        //     $table->timestamps();
 
-            $table->index('log_name');
-        });
+        //     $table->index('log_name');
+        // });
     }
 
     /**
