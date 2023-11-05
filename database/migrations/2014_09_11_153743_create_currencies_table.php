43a4556d226ef3592f733583493390830a37977a
Kawsar A <itskawsar@gmail.com>
2023-11-05 21:12:12 +0600

    change increments fields to id

--- database/migrations/2014_09_11_153743_create_currencies_table.php
+++ database/migrations/2014_09_11_153743_create_currencies_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('currencies', function (Blueprint $table) {
-            $table->increments('id');
+            // $table->increments('id');
+            $table->id();
             $table->string('country', 100);
             $table->string('currency', 100);
             $table->string('code', 25);
